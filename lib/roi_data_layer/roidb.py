7367c88520a080c35061143b6992a11351153b0a
Xinlei Chen <xinleic@cs.cmu.edu>
2017-04-08 13:07:40 -0400

    remove lingering imdb options.

--- lib/roi_data_layer/roidb.py
+++ lib/roi_data_layer/roidb.py
@@ -24,12 +24,12 @@ def prepare_roidb(imdb):
   recorded.
   """
   roidb = imdb.roidb
-  if not (imdb.name.startswith('vg') or imdb.name.startswith('avg') or imdb.name.startswith('coco')):
+  if not (imdb.name.startswith('coco')):
     sizes = [PIL.Image.open(imdb.image_path_at(i)).size
          for i in range(imdb.num_images)]
   for i in range(len(imdb.image_index)):
     roidb[i]['image'] = imdb.image_path_at(i)
-    if not (imdb.name.startswith('vg') or imdb.name.startswith('avg') or imdb.name.startswith('coco')):
+    if not (imdb.name.startswith('coco')):
       roidb[i]['width'] = sizes[i][0]
       roidb[i]['height'] = sizes[i][1]
     # need gt_overlaps as a dense array for argmax
