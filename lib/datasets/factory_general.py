594378c93ca9f5339d9b80e853bfe917455203d6
amlarraz <amlarraz@users.noreply.github.com>
2017-05-22 18:00:40 +0200

    Change factory.py to factory_general.py

--- lib/datasets/factory_general.py
+++ lib/datasets/factory_general.py
@@ -12,7 +12,7 @@ from __future__ import print_function
 
 __sets = {}
 from datasets.pascal_voc import pascal_voc
-from datasets.coco import coco
+#from datasets.coco import coco
 
 import numpy as np
 
@@ -23,16 +23,16 @@ for year in ['2007', '2012']:
     __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
 
 # Set up coco_2014_<split>
-for year in ['2014']:
-  for split in ['train', 'val', 'minival', 'valminusminival', 'trainval']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+#for year in ['2014']:
+#  for split in ['train', 'val', 'minival', 'valminusminival', 'trainval']:
+#    name = 'coco_{}_{}'.format(year, split)
+#    __sets[name] = (lambda split=split, year=year: coco(split, year))
 
 # Set up coco_2015_<split>
-for year in ['2015']:
-  for split in ['test', 'test-dev']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+#for year in ['2015']:
+#  for split in ['test', 'test-dev']:
+#    name = 'coco_{}_{}'.format(year, split)
+#    __sets[name] = (lambda split=split, year=year: coco(split, year))
 
 
 def get_imdb(name):
