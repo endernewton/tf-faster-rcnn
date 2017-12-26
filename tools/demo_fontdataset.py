0a0d28eaa5b87ddf8efc48093347154e675ce140
alifejon <alifejon@gmail.com>
2017-12-26 05:26:59 +0000

    add path setting

--- tools/demo_fontdataset.py
+++ tools/demo_fontdataset.py
@@ -15,6 +15,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import _init_paths
 from model.config import cfg
 from model.test import im_detect
 from model.nms_wrapper import nms
